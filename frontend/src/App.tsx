4a205945f7c124c338ccca2d0b2d66fb894abe9e
Nelo Sversuti <nelosversuti@gmail.com>
2022-07-14 10:41:08 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import NotificationButton from "./componets/NotificationButton"
+import Header from "./componets/Header"
+import SalesCard from "./componets/SalesCard"
+//import NotificationButton from "./componets/NotificationButton"
 
 function App() {
   return(
     <>
-      <h1>Olá Mundo!</h1>
-      <NotificationButton/>
+      <Header/>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
